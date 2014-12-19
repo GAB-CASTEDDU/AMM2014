4c0100f27a54a724216ad6df6084dfbed91494c0
Gabriele Trudu <gabriele.trudu@gmail.com>
2014-12-19 19:31:13 +0100

    session

--- Php/Amministratore/Gestisci.php
+++ Php/Amministratore/Gestisci.php
@@ -97,9 +97,9 @@ if($_COOKIE['tipo_utente']==1)
 else
 {
     $pagina_login="../Login.php";
-    $pagina_redirect="Amministratore/Gestisci.php";
+    $pagina_redirect=Amministratore/Gestisci.php;
 
-    setcookie("redirect", .$pagina_redirect, time()+300);
+    setcookie("redirect", $pagina_redirect, time()+300);
 
 	header("Location:".$pagina_login);
 }
