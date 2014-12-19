2797baf63faf63bc9d2b49e82d6e7d004cabfc66
Gabriele Trudu <gabriele.trudu@gmail.com>
2014-12-19 17:39:26 +0100

    session

--- Php/Login.php
+++ Php/Login.php
@@ -97,7 +97,7 @@ if(!isset($_SESSION["tipo"]))
 
                 <p>
                     <a href="http://validator.w3.org/check/referer">
-                    <img style="border:0;width:88px;height:31px" src="Immagini/html5.png" alt="HTML Valido!"/>
+                    <img style="border:0;width:88px;height:31px" src="../Immagini/html5.png" alt="HTML Valido!"/>
                     </a>
 
                     <a href="http://jigsaw.w3.org/css-validator/check/referer">
