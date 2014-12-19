bdcf79bae47ddd8cf19ef0b0f5a0f45497ce4eef
Gabriele Trudu <gabriele.trudu@gmail.com>
2014-12-19 20:21:31 +0100

    session

--- Php/Autentica.php
+++ Php/Autentica.php
@@ -3,8 +3,9 @@
 session_start();
 
 setcookie("redirect", null);
+echo($_COOKIE["redirect"]);
 
-$connessione_al_server=mysql_connect("localhost","truduGabriele","beluga874");
+/*$connessione_al_server=mysql_connect("localhost","truduGabriele","beluga874");
 
 if(!$connessione_al_server)
 {
