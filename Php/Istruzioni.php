f5679064da32ed2d99c7517c3856c90a2c7a128f
Gabriele Trudu <gabriele.trudu@gmail.com>
2014-12-20 11:27:02 +0100

    indentatura

--- Php/Istruzioni.php
+++ Php/Istruzioni.php
@@ -63,7 +63,7 @@ if(!isset($_COOKIE["tipo_utente"]))
 			<td id="center">
 				<h1 id="h1-istruzioni">Istruzioni</h1>
 
-                		<p>A4R nasce con lo scopo di simulare la creazione di un sito web per la compravendita di autovetture.</p>
+                		<p>A4R nasce con lo scopo di simulare la creazione di un sito web per la </br>compravendita di autovetture.</p>
 
                 		<p>Tale progetto prevedeva di soddisfare alcuni requisiti:</p>
 
