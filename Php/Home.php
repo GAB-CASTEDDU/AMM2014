4fb2e334c0085a05e769935af8233269d50730a1
Gabriele Trudu <gabriele.trudu@gmail.com>
2014-12-22 00:10:24 +0100

    mysql

--- Php/Home.php
+++ Php/Home.php
@@ -86,9 +86,9 @@ if(!isset($_COOKIE["tipo_utente"]))
                             if(isset($_GET["ricerca"]) && ($_GET["ricerca"]=="ok"))
                             {
                                 if(isset($_POST["marca"]))$_SESSION["marca"] = $_POST["marca"];
-                                $_SESSION["modello"] = "*";
+                                $_SESSION["modello"] = $_POST["modello"];
                                 $_SESSION["anno"] = $_POST["anno"];
-                                //$_SESSION["alimentazione"] = $_POST["alimentazione"];
+                                $_SESSION["alimentazione"] = $_POST["alimentazione"];
                                 $_SESSION["prezzo"] = $_POST["prezzo"];
                                 $_SESSION["chilometri"] = $_POST["chilometri"];
 
