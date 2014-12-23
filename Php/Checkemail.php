d1b2ce5317eb26fe41fc2073175ed804d00cc9a5
Gabriele Trudu <gabriele.trudu@gmail.com>
2014-12-23 15:35:39 +0100

    registrati

--- Php/Checkemail.php
+++ Php/Checkemail.php
@@ -1,22 +1,8 @@
 <?php
 
-$connessione_al_server = mysql_connect("localhost","truduGabriele","beluga874");
-
-if(!$connessione_al_server)
-{
-    die ("Errore: connessione non riuscita".mysql_error());
-}
 
 
-
-$db_selected = mysql_select_db("amm14_truduGabriele", $connessione_al_server);
-
-if(!$db_selected)
-{
-    die ("Errore: selezione del database errata ".mysql_error());
-}
-
-//$link = mysqli_connect("localhost", "root', 'password', 'db_name');
+$link = mysqli_connect("localhost", "truduGabriele", "beluga874", "users");
 
 $email = $_POST["email"];
 
@@ -26,7 +12,7 @@ $query = "SELECT email FROM users WHERE email='$email'";
 
 $result = mysql_query($connessione_al_server,$query);
 
-/*if(mysql_num_rows($result) > 0)
+if(mysql_num_rows($result) > 0)
 {
     echo '1';
 }
@@ -34,5 +20,5 @@ $result = mysql_query($connessione_al_server,$query);
 else
 {
     echo '0';
-}*/
+}
 ?>
