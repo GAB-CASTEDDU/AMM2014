8c9b40a1d1a025ba2e61ffc56ddac157c88792cd
Gabriele Trudu <gabriele.trudu@gmail.com>
2014-12-23 18:38:38 +0100

    registrati

--- Js/Checkemail.js
+++ Js/Checkemail.js
@@ -3,7 +3,7 @@ $(document).ready( function()
                     {
                         $("#emailok").keyup(function()
                                             {
-                                                var email = this.id;
+                                                var email = this.val();
 
                                                 $.ajax(
                                                 {
