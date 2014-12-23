d0cba5bf0a93d380bb66d84292a0e4c9d6f3c786
Gabriele Trudu <gabriele.trudu@gmail.com>
2014-12-23 11:55:13 +0100

    registrati

--- Php/Login.php
+++ Php/Login.php
@@ -66,7 +66,7 @@ if(!isset($_COOKIE["tipo_utente"]))
                             {
                             ?>
 
-                            <p>Combinazione E-mail - Password errata!</p>
+                            <p class="error">Combinazione E-mail - Password errata!</p>
 
                             <?
                                 setcookie("errlogin", null);
