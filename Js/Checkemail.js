441341a3b565fe0b0a31dc90863c95561490e2b2
Gabriele Trudu <gabriele.trudu@gmail.com>
2014-12-24 17:14:24 +0100

    ajax

--- Js/Checkemail.js
+++ Js/Checkemail.js
@@ -11,7 +11,7 @@ $(document).ready( function()
                                                     data: emailok,
                                                     success: function(response)
                                                                 {
-                                                                    if(response == "yes")
+                                                                    if(response == 1)
                                                                     {
                                                                         $("#checkemail").html("<font color='B20000'>Non disponibile</font>");
                                                                         $("#emailok").val("");
