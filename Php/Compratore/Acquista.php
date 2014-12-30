d20056c39e9f4b6fcaceb3c7d3c1dc46ace39043
Gabriele Trudu <gabriele.trudu@gmail.com>
2014-12-31 00:05:39 +0100

    carrello

--- Php/Compratore/Acquista.php
+++ Php/Compratore/Acquista.php
@@ -26,11 +26,11 @@ if($_COOKIE['tipo_utente']==3)
 
     while($row = mysql_fetch_object($queryvis))
     {
-        $querycom = "UPDATE utenti SET credito = credito - '".$row->prezzo."' WHERE email='".$row->carrello.compratore."'";
+        $querycom = "UPDATE utenti SET credito = credito - '".$row->auto.prezzo."' WHERE email='".$row->carrello.compratore."'";
 
-        $queryven = "UPDATE utenti SET credito = credito + '".$row->prezzo."' WHERE email='".$row->auto.venditore."'";
+        $queryven = "UPDATE utenti SET credito = credito + '".$row->auto.prezzo."' WHERE email='".$row->auto.venditore."'";
 
-        $queryaut = "UPDATE auto SET compratore = '".$row->compratore."' WHERE '".$row->carrello.compratore."' ='".$_COOKIE["utente"]."'";
+        $queryaut = "UPDATE auto SET compratore = '".$row->carrello.compratore."' WHERE '".$row->carrello.compratore."' ='".$_COOKIE["utente"]."'";
 
         $querycar = "DELETE FROM carrello WHERE '".$row->carrello.id."' = '".$row->auto.id."' AND compratore ='".$_COOKIE["utente"]."'";
 
