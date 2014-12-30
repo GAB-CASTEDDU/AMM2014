62fe56f66163dba4e8452fb9606fc43684c31088
Gabriele Trudu <gabriele.trudu@gmail.com>
2014-12-31 00:42:24 +0100

    carrello

--- Php/Compratore/Acquista.php
+++ Php/Compratore/Acquista.php
@@ -26,7 +26,7 @@ if($_COOKIE['tipo_utente']==3)
 
     while($row = mysql_fetch_object($queryvis))
     {
-        $querycom = "UPDATE utenti SET credito -= '".$row->prezzo ."' WHERE email='".$row->carrello.compratore ."'";
+        $querycom = "UPDATE utenti SET credito -= '".$row->prezzo ."' WHERE email='".$row->compratore ."'";
 
         $queryven = "UPDATE utenti SET credito += '".$row->prezzo ."' WHERE email='".$row->venditore ."'";
 
